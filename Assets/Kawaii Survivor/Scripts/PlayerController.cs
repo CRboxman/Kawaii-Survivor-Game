dcd4eef0119025553ffd2f003fc47fdba33fe160
tang tang <1524533474@qq.com>
2025-04-26 00:37:43 +0800

    暂时保存1

--- Assets/Kawaii Survivor/Scripts/PlayerController.cs
+++ Assets/Kawaii Survivor/Scripts/PlayerController.cs
@@ -1,21 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+
 
 public class PlayerController : MonoBehaviour
 {
+
     [SerializeField] private MobileJoystick playerJoystick;
+    [SerializeField]private float moveSpeed = 0.1f;
     private Rigidbody2D rig;
 
 
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+
     }
 
     void FixedUpdate()
     {
-        
-        rig.velocity = playerJoystick.GetMoveVector() ;
+
+        rig.velocity = playerJoystick.GetMoveVector()*moveSpeed*Time.deltaTime;
     }
+
+
 }
