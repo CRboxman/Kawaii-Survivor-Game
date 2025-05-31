6998bf2e495e573c20f757eaf5854a3e533bfecc
tang tang <1524533474@qq.com>
2025-05-31 23:22:21 +0800

    6.1 对象池的使用

--- Assets/Kawaii Survivor/Scripts/Effects/DamageTextManager.cs
+++ Assets/Kawaii Survivor/Scripts/Effects/DamageTextManager.cs
@@ -1,36 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class DamageTextManager : MonoBehaviour
 {
 
     [Header("Objects")]
     [SerializeField] private DamageText damageTextPrefab ;
+    private ObjectPool<DamageText> damageTextPool;
     private void Awake()
     {
-        Enemy.onDamageTaken += InstantiateDamageText;
+        Enemy.onDamageTaken += EnemyHitCallBack;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        damageTextPool = new ObjectPool<DamageText>(
+            CreateFunction,
+            ActionOnGet,
+            ActionOnRelease,
+            ActionOnDestroy
+        );
     }
 
-    // Update is called once per frame
-    void Update()
+    private DamageText CreateFunction()
     {
-        
+        return Instantiate(damageTextPrefab, transform);
     }
+    private void ActionOnGet(DamageText text)
+    {
+        text.gameObject.SetActive(true);
+    }
+    private void ActionOnRelease(DamageText text)
+    {
+        text.gameObject.SetActive(false);
+    }
+    private void ActionOnDestroy(DamageText text)
+    {
+        Destroy(text.gameObject);
+    }
+
     private void OnDestroy()
     {
-        Enemy.onDamageTaken -= InstantiateDamageText;
+        Enemy.onDamageTaken -= EnemyHitCallBack;
     }
-    private void InstantiateDamageText(float damage,Vector2 enemyPos)
+    private void EnemyHitCallBack(float damage,Vector2 enemyPos)
     {
+        DamageText damageTextInstance =damageTextPool.Get();
+
         Vector3 spawnPosition = enemyPos;
-        DamageText damageTextInstance = Instantiate(damageTextPrefab, spawnPosition, Quaternion.identity, transform);
+        damageTextInstance.transform.position = spawnPosition;
         damageTextInstance.PlayAnimate(damage);
+        //1秒之后释放（释放即返回到池中，失活那个对象）
+        LeanTween.delayedCall(1, () => damageTextPool.Release(damageTextInstance));
     }
 }
