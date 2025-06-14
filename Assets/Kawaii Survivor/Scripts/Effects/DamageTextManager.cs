f7e23799c7aa8a30c8f6f329677364f8b4deb1b3
tang tang <1524533474@qq.com>
2025-06-14 21:00:18 +0800

    紧急保存

--- Assets/Kawaii Survivor/Scripts/Effects/DamageTextManager.cs
+++ Assets/Kawaii Survivor/Scripts/Effects/DamageTextManager.cs
@@ -12,7 +12,7 @@ public class DamageTextManager : MonoBehaviour
     private ObjectPool<DamageText> damageTextPool;
     private void Awake()
     {
-        MeleeEnemy.onDamageTaken += EnemyHitCallBack;
+        Enemy.onDamageTaken += EnemyHitCallBack;
     }
 
     // Start is called before the first frame update
@@ -48,9 +48,9 @@ public class DamageTextManager : MonoBehaviour
     private void EnemyHitCallBack(float damage,Vector2 enemyPos)
     {
         DamageText damageTextInstance =damageTextPool.Get();
-
         Vector3 spawnPosition = enemyPos;
-        spawnPosition = Camera.main.WorldToScreenPoint(spawnPosition);
+
+        //spawnPosition = Camera.main.WorldToScreenPoint(spawnPosition);
         damageTextInstance.transform.position = spawnPosition;
         damageTextInstance.PlayAnimate(damage);
         //1秒之后释放（释放即返回到池中，失活那个对象）
